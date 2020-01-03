86c763eca5cf1e3d4ac3db2d692ac55bcb0c6fda
Max Jonghyuk Kim <hwaseen@gmail.com>
2020-01-03 16:26:11 +0900

    window.onpopstate로 app.js에서 클라이언트 렌더링 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import Home from './views/home'
+import About from './views/about'
 
-function App() {
-  return (
-    <div className="App">
-      hello world!
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    page: this.props.page,
+  };
+  componentDidMount() {
+    window.onpopstate = event => {
+      this.setState({ page: event.state });
+    };
+  }
+  onChangePage = e => {
+    const page = e.target.dataset.page;
+    window.history.pushState(page, '', `/${page}`);
+    this.setState({ page });
+  }
+  render() {
+    const { page } = this.state;
+    const PageComponent = page === 'home' ? Home : About;
+    return (
+      <div className="container">
+        <button data-page="home" onClick={this.onChangePage}>
+          Home
+        </button>
+        <button data-page="about" onClick={this.onChangePage}>
+          About
+        </button>
+        <PageComponent />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/views/about.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h3>This is about Page</h3>
+    </div>
+  )
+}
