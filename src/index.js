73fc56b82c5fcb55430992c5bf491e0b66c15fa3
Max Jonghyuk Kim <hwaseen@gmail.com>
2020-01-03 21:47:08 +0900

    express 서버에서 url 파싱해서 만든 문자열 정보 DATA_FROM_SERVER을 통해 전달하기 구현

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.hydrate(<App page="home" />, document.getElementById('root'));
+const initialData = window.__INITIAL_DATA__;
+
+ReactDOM.hydrate(<App page={initialData.page} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
 
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import path from 'path';
 import { renderToString } from 'react-dom/server';
 import React from 'react';
 import App from './App'
+import * as url from 'url';
 
 const app = express();
 const html = fs.readFileSync(
@@ -13,12 +14,15 @@ const html = fs.readFileSync(
 app.use('/dist', express.static('dist'));
 app.get('/favicon.ico', (req, res) => res.sendStatus(204));
 app.get('*', (req, res) => {
-  const renderString = renderToString(<App page="home" />);
+  const parsedUrl = url.parse(req.url, true);
+  const page = parsedUrl.pathname ? parsedUrl.pathname.substr(1) : 'home';
+  const renderString = renderToString(<App page={page} />);
+  const initialData = { page };
   const result = html.replace(
     '<div id="root"></div>',
-    `<div id="root">${renderString}</div>`,
-    // 맨 처음의 요청에 응답, div#root를 대체
-  );
+    `<div id="root">${renderString}</div>`)
+    .replace('__DATA_FROM_SERVER__', JSON.stringify(initialData));
+  // 맨 처음의 요청에 응답, div#root를 대체
   res.send(result)
 });
 app.listen(3000);
